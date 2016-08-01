d3a124fbda802caab8c211ec0fb9ad979e949b87
Jonathan Stowe <jns@gellyfish.co.uk>
2016-08-01 19:23:33 +0100

    Actually do something in the constructor

--- lib/Linux/Udev.pm
+++ lib/Linux/Udev.pm
@@ -3,6 +3,21 @@ use v6.c;
 use NativeCall;
 
 class Linux::Udev {
+    constant LIB = [ 'udev', v1 ];
+
+    class Context is repr('CPointer') {
+        sub udev_new() returns Context is native(LIB) { * }
+
+        method new() {
+            udev_new();
+        }
+    }
+
+    has Context $.context;
+
+    submethod BUILD() {
+        $!context = Context.new;
+    }
 
 
 }
