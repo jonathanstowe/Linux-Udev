b2cc14c17ff578f8cd22a5a95f59df7312c30448
Jonathan Stowe <jns@gellyfish.co.uk>
2016-08-04 09:25:26 +0100

    Add stubs for all the classes we need to start

--- lib/Linux/Udev.pm
+++ lib/Linux/Udev.pm
@@ -5,7 +5,23 @@ use NativeCall;
 class Linux::Udev {
     constant LIB = [ 'udev', v1 ];
 
-    class Context is repr('CPointer') {
+    class Context is Any is repr('CPointer') {
+        ...
+    }
+
+    class Enumerate is repr('CPointer') {
+
+    }
+
+    class ListEntry is repr('CPointer') {
+    
+    }
+
+    class Device is repr('CPointer') {
+
+    }
+
+    class Context  {
         sub udev_new() returns Context is native(LIB) { * }
 
         method new() {
